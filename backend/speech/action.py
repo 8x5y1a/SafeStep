37046d1a8c5616106bcd0a97fed03c47bc64eb5f
8x5y1a <jeff.gamache@icloud.com>
2026-01-17 11:48:24 -0500

    API Connection to frontend
    - Sends notification to frontend through websocket & api when help is requested
    - Uploaded things to github pages, will run api and python in local for simplicity.
    - Updated requirements

--- backend/speech/action.py
+++ backend/speech/action.py
@@ -1,5 +1,10 @@
 from backend.speech.textToSpeech import text_to_speech
 from backend.services.gemini import prompt_gemini
+from dotenv import load_dotenv
+import os
+import requests
+
+load_dotenv()
 
 prompt_context = "You will be given a transcript, from this transcript you must correctly select one of the following options:  1. Is the user asking to see what is around him (Usage of word like 'show me' or 'what is nearby', etc.) 2. Is the user asking for help (Words like 'I need help' or 'I need assistance', etc.) 3. Nothing above. You must return the appropriate prompt based on the selection. If 3: Answer: 'Nothing', If 2: Take the context of the transcript to say something along those lines: 'Someone is coming to help you' or 'Someone will come to assist you shortly', but make sure to personalize it. If 1: return 'Vision'. Do not add anything else to the answer of the prompt. The following text is the transcript: "
 
@@ -17,5 +22,6 @@ async def read_transcript(transcription: str):
         # TODO: send code to vision part
         return
 
+    requests.post(str(os.getenv("NGROK_URL")) + "/broadcast")  # json={"info": "TODO:"}
     print(answer)
     text_to_speech(answer)
