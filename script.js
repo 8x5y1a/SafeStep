75cca492c7fd93d56ef47563317ac72f6ead7afc
8x5y1a <jeff.gamache@icloud.com>
2026-01-17 13:35:28 -0500

    Changed map logic
    - Changed the map logic, instead of finding your location, I hardedcoded CRX location and made it that the person in need of help is in CRX.
    - Added logic behind "help person in need"

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ try {
 
 const notification = document.getElementById("notification");
 const request = document.getElementById("request");
+const helpPersonBtn = document.getElementById("helpPersonBtn");
+const crxCoordinates = [45.42193, -75.6817];
 
 function showNotification(
     message = "A user is requesting help!",
@@ -26,37 +28,28 @@ function showNotification(
     request.classList.remove("hidden");
     document.getElementById("no-requests").classList.add("hidden");
 
+    // Currently hardcoded the location of the client in need of help
+    L.marker(crxCoordinates)
+        .addTo(map)
+        .bindPopup("Person in need of help")
+        .openPopup();
+
+    map.setView(crxCoordinates, 18);
+
     setTimeout(() => {
         notification.classList.add("hidden");
     }, duration);
 }
 
+helpPersonBtn.addEventListener("click", () => {
+    map.setView(crxCoordinates, 20);
+});
+
 // ############# MAP LOGIC ##############
-const map = L.map("map").setView([0, 0], 2);
+const map = L.map("map").setView(crxCoordinates, 17);
 
 // OpenStreetMap
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
     attribution: "&copy; OpenStreetMap contributors",
 }).addTo(map);
-
-// Get user locaiton
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-        (pos) => {
-            const { latitude, longitude } = pos.coords;
-            map.setView([latitude, longitude], 13);
-
-            L.marker([latitude, longitude])
-                .addTo(map)
-                .bindPopup("You are here")
-                .openPopup();
-        },
-        (err) => {
-            console.error("Geolocation error:", err.message);
-            showNotification("Could not get your location", 3000);
-        },
-    );
-} else {
-    showNotification("Geolocation not supported", 3000);
-}
